c629b3a2b6c027ad3f70232c1fd02951d982907a
Kazimir <pankazimierzwlesie@protonmail.com>
2022-09-27 16:08:50 +0200

    main menu

---
+++ source/handlers/menu.py
@@ -0,0 +1,21 @@
+from aiogram import types, Dispatcher
+
+from source.keyboards.menu import menu_kb
+
+
+async def main_menu(cb: types.CallbackQuery):
+    text = [
+            "Notes by grn.",
+            "",
+            "",
+            "Just use it."
+            ]
+    await cb.message.edit_text(
+            text="\n".join(text),
+            reply_markup=menu_kb())
+
+
+def reg_main_menu(dp: Dispatcher):
+    dp.register_callback_query_handler(
+            main_menu,
+            text="menu")
