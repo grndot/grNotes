b29826ee347764ec6e8f35fd057710d09700d929
Kazimir <pankazimierzwlesie@protonmail.com>
2022-10-18 08:37:48 +0200

    changes in architechture

--- source/services/db/notes.py
+++ source/services/db/notes.py
@@ -1,6 +1,19 @@
-from sqlalchemy import update
+from sqlalchemy import select, update
+from sqlalchemy import func
 
 from source.models.database import Notes
+
+
+async def getCountNotes(
+        session,
+        user_id):
+    stmt = select(func.count()).select_from(
+            Notes).where(
+                    Notes.OwnerID == user_id)
+    result = await session.execute(stmt)
+    answer = result.first()
+    print(f"{user_id=} got {answer=} for query getCountNotes")
+    return answer[0]
 
 
 async def updateOwnerID(
