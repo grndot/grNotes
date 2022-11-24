6118d88b08df9b5e9794c7a77334c09e0186b63c
Kazimir <pankazimierzwlesie@protonmail.com>
2022-11-24 22:52:35 +0100

    global changes in note keyboard

--- source/keyboards/note.py
+++ source/keyboards/note.py
@@ -1,23 +1,25 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
-def note_kb() -> InlineKeyboardMarkup:
-    return InlineKeyboardMarkup(
-            row_width=2,
-            inline_keyboard=[
-                [
-                    InlineKeyboardButton(
+def note_kb(del_status=False) -> InlineKeyboardMarkup:
+    edit_note_button = InlineKeyboardButton(
                         text="Edit",
-                        callback_data="edit_note"),
-                    InlineKeyboardButton(
+                        callback_data="edit_note")
+    delete_note_button = InlineKeyboardButton(
                         text="Delete",
                         callback_data="del_note")
-                    ],
-                [
-                    InlineKeyboardButton(
-                        text="Back",
-                        callback_data="main_menu"
-                        )
-                    ]
-                ]
+    main_menu_button = InlineKeyboardButton(
+                        text="Ok" if del_status else "Back",
+                        callback_data="main_menu")
+    keyboard = InlineKeyboardMarkup(
+            row_width=2
             )
+    if del_status:
+        keyboard.insert(main_menu_button)
+    else:
+        keyboard.row(
+                edit_note_button,
+                delete_note_button)
+        keyboard.insert(main_menu_button)
+
+    return keyboard
