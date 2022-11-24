61d24d92f9e6e2ab7f4ef72128b0268177318afc
Kazimir <pankazimierzwlesie@protonmail.com>
2022-11-24 21:56:23 +0100

    add NoteState in show_note handler

--- source/handlers/menu/show_note.py
+++ source/handlers/menu/show_note.py
@@ -1,20 +1,25 @@
 from aiogram import types, Dispatcher
+from aiogram.dispatcher import FSMContext
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from source.keyboards.menu import show_note
 from source.keyboards.note import note_kb
 from source.services.db.notes import getNoteTextByNoteID
+from source.states.note import NoteState
 
 
 async def show_chosen_note(
         cb: types.CallbackQuery,
         callback_data: dict,
-        session: AsyncSession):
+        session: AsyncSession,
+        state: FSMContext):
 
     note_id = callback_data.get("note_id")
     note_text = await getNoteTextByNoteID(
             session=session,
             note_id=int(note_id))
+    await NoteState.ID.set()
+    await state.set_data({"NoteID": note_id})
     await cb.answer()
     await cb.message.edit_text(
             text=f"<code>{note_text}</code>",
@@ -24,5 +29,6 @@ async def show_chosen_note(
 def reg_show_note(dp: Dispatcher):
     dp.register_callback_query_handler(
             show_chosen_note,
-            show_note.filter())
+            show_note.filter(),
+            state="*")
 
