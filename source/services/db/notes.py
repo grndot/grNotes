ae9ce4b79a6fa07c4a7f20b97c5ffe742677947e
Kazimir <pankazimierzwlesie@protonmail.com>
2022-11-25 12:02:26 +0100

    delete Note func

--- source/services/db/notes.py
+++ source/services/db/notes.py
@@ -1,9 +1,69 @@
-from sqlalchemy import insert, select, update
+from sqlalchemy import delete, insert, select, update
 from sqlalchemy import func
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from source.models.database import Notes
 
+
+async def deleteNote(
+        session: AsyncSession,
+        note_id: int):
+    stmt = delete(Notes).where(Notes.ID == note_id)
+    await session.execute(stmt)
+    await session.commit()
+
+
+async def getCountNotes(
+        session: AsyncSession,
+        owner_id: int):
+    stmt = select(func.count()).select_from(
+            Notes).where(
+                    Notes.OwnerID == owner_id)
+    result = await session.execute(stmt)
+    answer = result.first()
+    print(f"{owner_id=} got {answer=} for query getCountNotes")
+    return answer[0]
+
+
+async def getNotesTitleAndIDByOwnerID(
+        session: AsyncSession,
+        owner_id: int):
+ 
+    class Note:
+        def __init__(self, title: str, id_note: int) -> None:
+    
+            self.title = title
+            self.id = id_note
+
+    stmt = select(Notes.Title, Notes.ID).where(
+            Notes.OwnerID==owner_id)
+    result = await session.execute(stmt)
+    arrow = result.all()
+    answer = [Note(title=item[0], id_note=item[1]) for item in arrow]
+    print(f"{owner_id=} got Titles and IDs his notes")
+    return answer
+
+
+async def getNoteTitleAndTextByNoteID(
+        session: AsyncSession,
+        note_id: int):
+    
+    class Note:
+        def __init__(self, title: str, text: str):
+
+            self.title = title
+            self.text = text
+
+    stmt = select(
+            Notes.Title,
+            Notes.Text).where(
+            Notes.ID == note_id)
+    result = await session.execute(stmt)
+    init_data = result.first()
+    return Note(
+            title=init_data[0],
+            text=init_data[1])
+    
 
 async def insertNewNote(
         session: AsyncSession,
@@ -20,45 +80,6 @@ async def insertNewNote(
     return result.scalars()
 
 
-async def getNotesDataByOwnerID(
-        session: AsyncSession,
-        owner_id: int):
- 
-    class Note:
-        def __init__(
-                self,
-                title: str,
-                id_note: int,
-                text: str) -> None:
-    
-            self.title = title
-            self.id = id_note
-            self.text = text
-
-    stmt = select(Notes.Title, Notes.ID, Notes.Text).where(
-            Notes.OwnerID==owner_id)
-    result = await session.execute(stmt)
-    arrow = result.all()
-    answer = [Note(
-        title=item[0],
-        id_note=item[1],
-        text=item[2]) for item in arrow]
-    print(f"{owner_id=} got Titles and IDs his notes")
-    return answer
-
-
-async def getCountNotes(
-        session: AsyncSession,
-        owner_id: int):
-    stmt = select(func.count()).select_from(
-            Notes).where(
-                    Notes.OwnerID == owner_id)
-    result = await session.execute(stmt)
-    answer = result.first()
-    print(f"{owner_id=} got {answer=} for query getCountNotes")
-    return answer[0]
-
-
 async def updateOwnerID(
         session: AsyncSession,
         old_id: int,
@@ -69,3 +90,5 @@ async def updateOwnerID(
     await session.execute(stmt)
     await session.commit()
     print(f"Notes from {old_id=} has been provided for {new_id=}")
+
+
